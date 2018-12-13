61caab3503a8d2339f55bdb36f4acc1404c81a20
Alexi Christakis <alexi.christakis@gmail.com>
2018-12-13 15:36:25 -0500

    added zoom to time series

--- src/components/Anayltics/TimeSeries.js
+++ src/components/Anayltics/TimeSeries.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { VictoryLabel, VictoryChart, VictoryTheme, VictoryAxis, VictoryLine } from "victory";
+import _ from "lodash";
+import moment from "moment";
+import {
+	VictoryLabel,
+	VictoryZoomContainer,
+	VictoryChart,
+	VictoryTheme,
+	VictoryAxis,
+	VictoryLine
+} from "victory";
 
 const Wrapper = styled.div`
 	width: 100%;
@@ -10,7 +19,38 @@ const Wrapper = styled.div`
 `;
 
 class TimeSeries extends Component {
-	render() {
+	state = {
+		zoomedXDomain: [],
+		combined_dataset: [],
+		positive: [],
+		negative: []
+	};
+
+	componentDidMount() {
+		this.updateData([-1 * Infinity, Infinity], () =>
+			this.setState({ entireDomain: this.getEntireDomain })
+		);
+	}
+
+	getEntireDomain() {
+		const { combined_dataset } = this.state;
+		return {
+			y: [_.minBy(combined_dataset, d => d.y).y, _.maxBy(combined_dataset, d => d.y).y],
+			x: [combined_dataset[0].x, _.last(combined_dataset).x]
+		};
+	}
+
+	getData() {
+		const { zoomedXDomain } = this.state;
+		this.updateData(zoomedXDomain);
+	}
+
+	filter = (entry, domain) => {
+		const ts = moment(entry.x).unix();
+		return ts >= domain[0] && ts <= domain[1];
+	};
+
+	updateData = (domain, callback) => {
 		const { time_series } = this.props.data;
 
 		let combined_dataset = time_series
@@ -21,7 +61,10 @@ class TimeSeries extends Component {
 					y: entry.score
 				};
 			})
-			.reverse();
+			.reverse()
+			.filter(entry => this.filter(entry, domain));
+
+		console.log("combined:", combined_dataset);
 
 		let positive = time_series
 			.map(entry => {
@@ -31,7 +74,8 @@ class TimeSeries extends Component {
 					y: entry.pos
 				};
 			})
-			.reverse();
+			.reverse()
+			.filter(entry => this.filter(entry, domain));
 
 		let negative = time_series
 			.map(entry => {
@@ -41,11 +85,30 @@ class TimeSeries extends Component {
 					y: entry.neg
 				};
 			})
-			.reverse();
+			.reverse()
+			.filter(entry => this.filter(entry, domain));
+
+		this.setState({ combined_dataset, positive, negative }, callback);
+	};
+
+	onDomainChange = domain => {
+		this.setState({
+			zoomedXDomain: domain.x
+		});
+	};
+
+	render() {
+		const { combined_dataset, positive, negative } = this.state;
+		console.log(combined_dataset);
 
 		return (
 			<Wrapper>
-				<VictoryChart theme={VictoryTheme.grayscale}>
+				<VictoryChart
+					theme={VictoryTheme.grayscale}
+					containerComponent={
+						<VictoryZoomContainer zoomDimension="x" onZoomDomainChange={this.onDomainChange} />
+					}
+				>
 					<VictoryLine
 						data={combined_dataset}
 						domain={{ y: [-1, 1] }}
@@ -70,7 +133,12 @@ class TimeSeries extends Component {
 						}}
 					/>
 				</VictoryChart>
-				<VictoryChart theme={VictoryTheme.grayscale}>
+				<VictoryChart
+					theme={VictoryTheme.grayscale}
+					containerComponent={
+						<VictoryZoomContainer zoomDimension="x" onZoomDomainChange={this.onDomainChange} />
+					}
+				>
 					<VictoryLine
 						data={positive}
 						style={{
